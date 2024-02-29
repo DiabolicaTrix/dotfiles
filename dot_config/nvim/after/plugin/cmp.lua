7805032460173a84df2207e1c9d5d672f0a70932
Alexy Mantha <alexy@mantha.dev>
2024-02-28 21:55:37 -0500

    fix cmp auto-select

--- dot_config/nvim/after/plugin/cmp.lua
+++ dot_config/nvim/after/plugin/cmp.lua
@@ -27,7 +27,7 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
         { name = 'copilot' },
