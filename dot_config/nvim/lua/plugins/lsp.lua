8f699b31ca58b570f27c45a1eb6993d1bef11011
Alexy Mantha <alexy@mantha.dev>
2024-03-24 23:43:27 -0400

    configure trouble
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -206,6 +206,44 @@ return {
 	{
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{
+				"<Leader>xx",
+				function()
+					require("trouble").toggle()
+				end,
+			},
+			{
+				"<Leader>xw",
+				function()
+					require("trouble").toggle("workspace_diagnostics")
+				end,
+			},
+			{
+				"<Leader>xd",
+				function()
+					require("trouble").toggle("document_diagnostics")
+				end,
+			},
+			{
+				"<Leader>xq",
+				function()
+					require("trouble").toggle("quickfix")
+				end,
+			},
+			{
+				"<Leader>xl",
+				function()
+					require("trouble").toggle("loclist")
+				end,
+			},
+			{
+				"gR",
+				function()
+					require("trouble").toggle("lsp_references")
+				end,
+			},
+		},
 		opts = {},
 	},
 }
