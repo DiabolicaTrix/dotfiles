b961f1cf887b8662014d54282e0cdd0242d6d046
Alexy Mantha <alexy@mantha.dev>
2024-09-21 18:46:31 -0400

    setup java :(:
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -283,56 +283,50 @@ return {
 		"mfussenegger/nvim-jdtls",
 		ft = "java",
 		config = function()
-			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-			local workspace_dir = '/home/alexy/.cache/jdtls/workspaces/' .. project_name
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "java",
+				callback = function()
+					local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+					local workspace_dir = vim.env.HOME .. '/.cache/jdtls/workspaces/' .. project_name
+					local config_dir = vim.env.HOME .. '/.cache/jdtls/configuration'
+					local jdtls_path = vim.fn.expand('$HOME/.nix-profile/bin/jdtls.py')
 
-			local config = {
-				-- The command that starts the language server
-				-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-				cmd = {
-					'java',
+					local config = {
+						-- The command that starts the language server
+						-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+						cmd = {
+							'jdtls',
+							'-configuration', config_dir,
+							'-data', workspace_dir
+						},
 
-					'-Declipse.application=org.eclipse.jdt.ls.core.id1',
-					'-Dosgi.bundles.defaultStartLevel=4',
-					'-Declipse.product=org.eclipse.jdt.ls.core.product',
-					'-Dlog.protocol=true',
-					'-Dlog.level=ALL',
-					'-Xmx1g',
-					'--add-modules=ALL-SYSTEM',
-					'--add-opens', 'java.base/java.util=ALL-UNNAMED',
-					'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+						root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
 
-					'-jar', '/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+						-- Here you can configure eclipse.jdt.ls specific settings
+						-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+						-- for a list of options
+						settings = {
+							java = {
+							}
+						},
 
-					'-configuration', '/usr/share/java/jdtls/config_linux',
-
-					'-data', workspace_dir
-				},
-
-				root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
-
-				-- Here you can configure eclipse.jdt.ls specific settings
-				-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-				-- for a list of options
-				settings = {
-					java = {
+						-- Language server `initializationOptions`
+						-- You need to extend the `bundles` with paths to jar files
+						-- if you want to use additional eclipse.jdt.ls plugins.
+						--
+						-- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+						--
+						-- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+						init_options = {
+							bundles = {}
+						},
 					}
-				},
-
-				-- Language server `initializationOptions`
-				-- You need to extend the `bundles` with paths to jar files
-				-- if you want to use additional eclipse.jdt.ls plugins.
-				--
-				-- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-				--
-				-- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-				init_options = {
-					bundles = {}
-				},
-			}
-			-- This starts a new client & server,
-			-- or attaches to an existing client & server depending on the `root_dir`.
-			require('jdtls').start_or_attach(config)
+					-- This starts a new client & server,
+					-- or attaches to an existing client & server depending on the `root_dir`.
+					require('jdtls').start_or_attach(config)
+				end,
+				group = vim.api.nvim_create_augroup("jdtls", {}),
+			})
 		end,
 	}
 }
