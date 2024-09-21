5f500844056814bbd222ac65fc9f3df5b8520440
Alexy Mantha <alexy@mantha.dev>
2024-09-21 15:35:01 -0400

    fix tsserver
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -87,7 +87,7 @@ return {
 					-- Languages
 					"gopls",
 					"pyright",
-					"tsserver",
+					"ts_ls",
 					"lua_ls",
 					"rust_analyzer",
 					-- Tools
@@ -164,12 +164,12 @@ return {
 						})
 					end,
 
-					["tsserver"] = function()
+					["ts_ls"] = function()
 						local lspconfig = require("lspconfig")
 						local mason_registry = require('mason-registry')
 						local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
 
-						lspconfig.tsserver.setup({
+						lspconfig.ts_ls.setup({
 							init_options = {
 								plugins = {
 									{
